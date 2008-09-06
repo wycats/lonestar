7cc6b7ee805a7b5e37713f02fd7208dfa4ca2eba
Yehuda Katz <wycats@gmail.com>
2008-09-05 23:37:21 -0500

    Passing hide input on escape

--- public/slices/merb_screw_unit/javascripts/application.js
+++ public/slices/merb_screw_unit/javascripts/application.js
@@ -1,5 +1,9 @@
 jQuery(function($) {
   $("p.edit_in_place").livequery("click", function() {
-    $(this).hide().after("<input type='text' name='val' />");
+    $(this).hide().after("<input type='text' class='editor' name='val' />");
   });
+  
+  $("input.editor").livequery("keydown", function(e) {
+    if(e.keyCode == 27) $(this).remove();
+  })
 });
