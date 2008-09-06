e21ad7c56799325f6b7a02ea77946ef6536a1347
Yehuda Katz <wycats@gmail.com>
2008-09-06 00:36:08 -0500

    Move cancel to a custom event

--- public/slices/merb_screw_unit/javascripts/application.js
+++ public/slices/merb_screw_unit/javascripts/application.js
@@ -1,21 +1,24 @@
 jQuery(function($) {
   $("p.edit_in_place").livequery("click", function() {
     $(this).hide().after(
-      "<p><input type='text' class='editor' name='val' />" +
+      "<p class='in_place_form'><input type='text' class='editor' name='val' />" +
       "<input type='submit' class='editor' />" +
       "<input type='button' class='editor cancel' /></p>"
     );
   });
   
+  $("p.in_place_form").livequery("cancel.editor", function() {
+    $(this).prev().show();
+    $(this).remove();
+  })
+  
   $("input.editor").livequery("keydown", function(e) {
     if(e.keyCode == 27) {
-      $(this).parent().prev().show();
-      $(this).parent().remove();
+      $(this).parent().trigger("cancel.editor")
     }
   });
   
   $("input.editor.cancel").livequery("click", function() {
-    $(this).parent().prev().show();
-    $(this).parent().remove();    
+    $(this).parent().trigger("cancel.editor")
   })
 });
