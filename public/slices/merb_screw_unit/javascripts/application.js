0e396c798dc9e722a31fdd41f4ff138ced0e985d
Yehuda Katz <wycats@gmail.com>
2008-09-06 02:08:56 -0500

    pass clicking submit

--- public/slices/merb_screw_unit/javascripts/application.js
+++ public/slices/merb_screw_unit/javascripts/application.js
@@ -4,8 +4,8 @@ jQuery(function($) {
   $("p.edit_in_place").livequery("click", function() {
     $(this).hide().after(
       "<p class='in_place_form'><input type='text' class='editor' name='val' />" +
-      "<input type='submit' class='editor' />" +
-      "<input type='button' class='editor cancel' /></p>"
+      "<input type='submit' class='editor' value='Submit' />" +
+      "<input type='button' class='editor cancel' value='Cancel' /></p>"
     );
   });
   
@@ -13,20 +13,28 @@ jQuery(function($) {
     $(this).prev().show();
     $(this).remove();
   })
+
+  $("p.in_place_form").livequery("submit.editor", function(e, self) {
+    var url = self.parent().prev().metadata().put;
+    $.post(url, {val: self.val()}, function(json) {
+      if(json.success) {
+        self.parent().prev().html(json.result).end().trigger("cancel.editor");
+      }
+    }, "json");
+  })
   
-  $("input.editor").livequery("keydown", function(e) {
+  $("input.editor:text").livequery("keydown", function(e) {
     var self = $(this);
     if(e.keyCode == 27) {
       self.parent().trigger("cancel.editor")
     } else if(e.keyCode == 13) {
-      var url = self.parent().prev().metadata().put;
-      $.post(url, {val: self.val()}, function(json) {
-        if(json.success) {
-          self.parent().prev().html(json.result).end().trigger("cancel.editor");
-        }
-      }, "json");
+      self.parent().trigger("submit.editor", [self])
     }
   });
+  
+  $("input.editor:submit").livequery("click", function() {
+    $(this).parent().trigger("submit.editor", [$(this).prev()])
+  })
   
   $("input.editor.cancel").livequery("click", function() {
     $(this).parent().trigger("cancel.editor")
